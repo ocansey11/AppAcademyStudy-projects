c0a42fb1e7d2255669a04408c33a3493a4478dc0
kevin ocansey <kevinocansey11@gmail.com>
2022-12-08 10:29:20 +0000

    update

--- tic-tac-toe-refresh-starter-master/starter/class/cursor.js
+++ tic-tac-toe-refresh-starter-master/starter/class/cursor.js
@@ -24,30 +24,44 @@ class Cursor {
 
   up() {
     // Move cursor up
+    this.resetBackgroundColor()
     if(this.row > 0){
       this.row -= 1
     }
+
+    this.setBackgroundColor();
+    Screen.render();
   }
 
   down() {
     // Move cursor down
+    this.resetBackgroundColor()
     if(this.row < this.numRows - 1){
       this.row += 1
     }
+
+    this.setBackgroundColor();
+    Screen.render();
   }
 
   left() {
     // Move cursor left
+    this.resetBackgroundColor()
     if(this.col > 0){
       this.col -= 1
     }
+    this.setBackgroundColor();
+    Screen.render();
   }
 
   right() {
     // Move cursor right
+    this.resetBackgroundColor()
     if(this.col < this.numRows - 1){
       this.col += 1
     }
+    this.setBackgroundColor();
+    Screen.render();
   }
 }
 module.exports = Cursor;
